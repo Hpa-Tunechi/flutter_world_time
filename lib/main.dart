f0072a60a7487453c2042b43669a3935f82e77e1
hpa <hriatahpa4@gmail.com>
2020-12-12 15:25:19 +0530

    first Flutter commit 12/12/2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_world_time/pages/choose_location.dart';
 import 'package:flutter_world_time/pages/home.dart';
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 class Loading extends StatefulWidget {
@@ -17,6 +18,7 @@ class _LoadingState extends State<Loading> {
       'location': instance.location,
       'flag': instance.flag,
       'time': instance.time,
+      'isDaytime': instance.isDaytime,
     });
   }
 
@@ -31,9 +33,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Text('loading'),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+          child: SpinKitFoldingCube(
+            color: Colors.white,
+            size: 50.0,
+          ),
       ),
     );
   }
